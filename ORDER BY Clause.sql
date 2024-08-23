2e542e1b48fe8236a408d46dadb723d85561e891
Chandana Adusumalli <a.chandana1709@gmail.com>
2024-08-22 22:38:04 -0500

    MySQL

---
+++ ORDER BY Clause.sql
@@ -0,0 +1,34 @@
+-- ORDER BY CLAUSE--
+
+SELECT *
+FROM parks_and_recreation.employee_demographics
+ORDER BY gender
+;
+
+SELECT *
+FROM parks_and_recreation.employee_demographics
+ORDER BY age
+;
+
+SELECT *
+FROM parks_and_recreation.employee_demographics
+ORDER BY age, gender
+;
+
+SELECT *
+FROM parks_and_recreation.employee_demographics
+ORDER BY gender, age
+;
+
+SELECT *
+FROM parks_and_recreation.employee_demographics
+ORDER BY gender, age DESC
+;
+
+SELECT *
+FROM parks_and_recreation.employee_demographics
+ORDER BY 5,4
+;
+
+
+
