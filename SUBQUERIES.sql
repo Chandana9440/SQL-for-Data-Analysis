9496c40d9d92b28d699e62697216a2129c53b587
Chandana Adusumalli <a.chandana1709@gmail.com>
2024-09-02 23:17:05 -0500

    MySQL

--- SUBQUERIES.sql
+++ SUBQUERIES.sql
@@ -1,4 +1,9 @@
 -- SUBQUERIES --
+-- Subqueries in MySQL are queries nested inside another query. 
+-- They allow you to execute a query to return data that will be used by the main query. 
+-- Subqueries can be used in various clauses, such as SELECT, FROM, WHERE, and HAVING, 
+-- and are helpful for filtering data, performing calculations, or manipulating data without the need to create temporary tables.
+
 SELECT * FROM employee_demographics;
 SELECT * FROM employee_salary;
 
